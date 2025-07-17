f43d8467da15ceee8134ac00b75f39396f54776d
Bidibulle <remy.delepaule@gmail.com>
2025-07-18 01:17:08 +0200

    feat(backend): add auth middleware

--- backend/src/routes/games.js
+++ backend/src/routes/games.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const gameController = require("../controllers/gameController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.get("/", gameController.findAll);
 router.get("/:id", gameController.findById);
 
-router.post("/", gameController.create);
+router.post("/", authMiddleware, gameController.create);
 
-router.put("/:id", gameController.update);
+router.put("/:id", authMiddleware, gameController.update);
 
-router.delete("/:id", gameController.delete);
+router.delete("/:id", authMiddleware, gameController.delete);
 
 module.exports = router;
--- backend/src/routes/players.js
+++ backend/src/routes/players.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const playerController = require("../controllers/playerController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.get("/", playerController.findAll);
 router.get("/:id", playerController.findById);
 
-router.post("/", playerController.create);
+router.post("/", authMiddleware, playerController.create);
 
-router.put("/:id", playerController.update);
+router.put("/:id", authMiddleware, playerController.update);
 
-router.delete("/:id", playerController.delete);
+router.delete("/:id", authMiddleware, playerController.delete);
 
 module.exports = router;
